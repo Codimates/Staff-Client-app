f0ce9e6b089f14d3c2dc8b9decfd8ae30c8eb27b
Janod Abesekara <125848373+JanodAbesekara@users.noreply.github.com>
2024-12-01 17:03:05 +0530

    add the notifacition part

--- src/pages/adminPages/Adminlanding.js
+++ src/pages/adminPages/Adminlanding.js
@@ -1,15 +1,23 @@
-import React, { useContext } from 'react'
-import { UserContext } from '../../context/UserContext'
+import React, { useContext} from "react";
+import { UserContext } from "../../context/UserContext";
+
+
 
 export default function Adminlanding() {
-  const {user, logout} = useContext(UserContext)
+
+
+  
+  const { user, logout } = useContext(UserContext);
+
+  
   return (
     <div>
-      <h1>admin</h1>
-      <button onClick={logout}>
-        Logout
+      <button className="bg-black ">
+        <a href="/Notifacition"> Hellow</a>
       </button>
+      <h1>admin</h1>
+      <button onClick={logout}>Logout</button>
       <h1>{user.fname}</h1>
     </div>
-  )
+  );
 }
