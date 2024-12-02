7774db2699837734421714504b985f91ae9cebad
TharinduDilshanEkanayake <ekanayakeemtd.21@uom.lk>
2024-12-02 21:25:38 +0530

    update inventory nav and head

--- src/componnets/inventorymanager/InventorymanagerNav.js
+++ src/componnets/inventorymanager/InventorymanagerNav.js
@@ -1,12 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import { MdDashboard } from 'react-icons/md';
 import { FaUserEdit } from 'react-icons/fa';
 import { MdOutlineInventory } from 'react-icons/md';
+import { UserContext } from '../../context/UserContext';
 
 export default function InventorymanagerNav() {
     const [activeItem, setActiveItem] = useState(null);
+    const { user, logout } = useContext(UserContext);
 
     const location = useLocation();
 
@@ -28,9 +30,19 @@ export default function InventorymanagerNav() {
     }, [location.pathname]);
 
     return (
-        <div className='h-screen  border-r-2 lg:w-[320px] sm:w-fit md:w-72 w-fit bg-gray-100'>
-            <div className='justify-center'>
-                <nav>
+        <div className='h-screen flex flex-col border-r-2 lg:w-[320px] sm:w-fit md:w-72 w-fit bg-gray-100'>
+            <div className='flex-grow pt-[60px]'>
+                <div className=' justify-center pb-[60px]'>
+                    <div className='flex justify-center'>
+                       <img src={user.image} alt='user' className='border-2 rounded-full w-[250px]'></img> 
+                    </div>
+                    
+                <h1 className='pt-2'><strong><span className='pr-1'>{user.fname}</span>{user.lname}</strong></h1>
+                </div>
+                
+                    
+                
+                <nav className='flex-grow'>
                     <ul className='text-left'>
                         <Link to='/inventorymanagerdash'>
                             <li>
@@ -44,17 +56,13 @@ export default function InventorymanagerNav() {
                                     <div className='mr-8'>
                                         <MdDashboard
                                             size={20}
-                                            className={`${
-                                                activeItem === 1 ? 'text-black' : 'text-black'
-                                            }`}
+                                            className='text-black'
                                         />
                                     </div>
                                     <div>
                                         <label
                                             style={{ fontSize: '18px' }}
-                                            className={`${
-                                                activeItem === 1 ? 'text-black' : 'text-black'
-                                            }`}
+                                            className='text-black'
                                         >
                                             Dashboard
                                         </label>
@@ -74,17 +82,13 @@ export default function InventorymanagerNav() {
                                     <div className='mr-8'>
                                         <MdOutlineInventory
                                             size={20}
-                                            className={`${
-                                                activeItem === 2 ? 'text-black' : 'text-black'
-                                            }`}
+                                            className='text-black'
                                         />
                                     </div>
                                     <div>
                                         <label
                                             style={{ fontSize: '18px' }}
-                                            className={`${
-                                                activeItem === 2 ? 'text-black' : 'text-black'
-                                            }`}
+                                            className='text-black'
                                         >
                                             Inventory
                                         </label>
@@ -104,19 +108,15 @@ export default function InventorymanagerNav() {
                                     <div className='mr-8'>
                                         <FaUserEdit
                                             size={20}
-                                            className={`${
-                                                activeItem === 3 ? 'text-black' : 'text-black'
-                                            }`}
+                                            className='text-black'
                                         />
                                     </div>
                                     <div>
                                         <label
                                             style={{ fontSize: '18px' }}
-                                            className={`${
-                                                activeItem === 3 ? 'text-black' : 'text-black'
-                                            }`}
+                                            className='text-black'
                                         >
-                                            Profile
+                                            Profile Settings
                                         </label>
                                     </div>
                                 </div>
@@ -124,6 +124,14 @@ export default function InventorymanagerNav() {
                         </Link>
                     </ul>
                 </nav>
+            </div>
+            <div className='flex justify-center pb-6'>
+                <button 
+                    onClick={logout} 
+                    className='w-[200px] py-3 text-white bg-black rounded-lg'
+                >
+                    Logout
+                </button>
             </div>
         </div>
     );
--- src/pages/inventorymanager/Profile.js
+++ src/pages/inventorymanager/Profile.js
@@ -1,13 +1,46 @@
 import React from 'react'
 import InventorymanagerNav from '../../componnets/inventorymanager/InventorymanagerNav'
+import InventoryManagerHeadBar from '../../componnets/inventorymanager/InventoryManagerHeadBar'
+
+
 
 export default function Profile() {
+ 
   return (
-    <div>
-      <div>
-        <InventorymanagerNav/>
-      </div>
 
+    <div>
+      <div className='flex '>
+        <div>
+          <InventorymanagerNav/>
+        </div>
+        <div className='w-screen'>
+          
+          <div className='flex justify-between pt-8 pb-8 pl-5'>
+                <div className=''>
+                  <h1 className='text-4xl'>Profile Settings</h1>
+                </div>
+                
+                <div className='mr-5'>
+                  <InventoryManagerHeadBar/>
+                </div>
+          </div>
+          <div >
+
+            
+           
+            <div>
+            <h1>w</h1>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+
+
+    
+    
   )
 }
+
+
